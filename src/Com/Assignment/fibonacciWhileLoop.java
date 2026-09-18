d47f1b7a820006826cbdaa04fbe46dcd683169d1
DevendraSinghRanaDS <devendrasinghranadsr@gmail.com>
2026-09-18 16:31:27 +0530

    java codes practice for beginner, here fibonacci series using while loop

---
+++ src/Com/Assignment/fibonacciWhileLoop.java
@@ -0,0 +1,20 @@
+package Com.Assignment;
+import java.util.Scanner;
+public class fibonacciWhileLoop {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int count=0;
+        int a=0;
+        int b=1;
+        while(count<n){
+            System.out.print(a +" ");
+            int c=a+b;
+            a=b;
+            b=c;
+
+            count++;
+        }
+
+    }
+}
