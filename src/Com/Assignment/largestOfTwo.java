7c43375d68db805677f01d2eaa77e0020bbd9dbb
DevendraSinghRanaDS <devendrasinghranadsr@gmail.com>
2026-09-18 17:00:22 +0530

    here is an easy if else code to find largest of two numbers input by the user

---
+++ src/Com/Assignment/largestOfTwo.java
@@ -0,0 +1,18 @@
+package Com.Assignment;
+import java.util.Scanner;
+public class largestOfTwo {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("enter num1: ");
+        float num1 = in.nextFloat();
+        System.out.print("enter num2: ");
+        float num2 = in.nextFloat();
+
+        if(num1>num2){
+            System.out.println("num1 is greater than num2 that is :"+num1);
+        } else{
+            System.out.println("num2 is greater than num1 that is :"+num2);
+        }
+    }
+}
