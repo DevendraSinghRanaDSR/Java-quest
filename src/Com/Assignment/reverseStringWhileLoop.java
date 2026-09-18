aa92609b499fdf9658e356d4dfb314efb02de4fc
DevendraSinghRanaDS <devendrasinghranadsr@gmail.com>
2026-09-18 17:04:11 +0530

    here is an easy code using while loop(to reverse each character one by one ) to check if a string is a palindrome or not . A palindrome is a string which is same as the reverse of itself, for example : abcba is a palindrome.

---
+++ src/Com/Assignment/reverseStringWhileLoop.java
@@ -0,0 +1,21 @@
+package Com.Assignment;
+import java.util.Scanner;
+public class reverseStringWhileLoop {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String a = in.nextLine();
+        String reversed = "";
+
+        int b=a.length();
+        while((b-1)>=0){
+            reversed=reversed+a.charAt(b-1);
+            b--;
+        }
+        System.out.println(reversed);
+        if(a.equals(reversed)){
+            System.out.print("palindrome");
+        }else{
+            System.out.print("not palindrome");
+        }
+    }
+}
